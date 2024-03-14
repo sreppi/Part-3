cf3b229f22ffdafa6e426ca4546f3a3fbc93dd5a
DESKTOP-11JJPAL\Calvin <calvinip@live.ca>
2024-03-13 22:18:32 -0400

    Task 4b completed poorly. CharacterControl script creates multiple NullReferences. Will need assistance.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,10 +1,14 @@
-using System;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI className;
     public static Villager SelectedVillager { get; private set; }
     public static void SetSelectedVillager(Villager villager)
     {
@@ -14,6 +18,13 @@ public class CharacterControl : MonoBehaviour
         }
         SelectedVillager = villager;
         SelectedVillager.Selected(true);
+        Debug.Log(SelectedVillager.ToString());
     }
-    
+
+    void FixedUpdate()
+    {
+        className = GetComponent<TextMeshProUGUI>();
+        className.text = SelectedVillager.ToString(); // I can't seem to get this to work outside of the script. This is causing a lot of issues.
+    }
+
 }
---
+++ Assets/Week 9/Scripts/ClassNameText.cs
@@ -0,0 +1,12 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ClassNameText : MonoBehaviour
+{
+
+}
