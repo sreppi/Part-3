1d9abf5a45dfdccf22728fa92ba7b6752025825d
DESKTOP-11JJPAL\Calvin <calvinip@live.ca>
2024-03-21 10:59:05 -0400

    Task 4b completed; ability to scale selected characters with slider.

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterControl : MonoBehaviour
@@ -8,6 +9,10 @@ public class CharacterControl : MonoBehaviour
     public TMPro.TextMeshProUGUI currentSelection;
     public static CharacterControl Instance;
     public static Villager SelectedVillager { get; private set; }
+    float interpolation;
+    public Vector3 startSize = new Vector3 (0.5f, 0.5f, 1);
+    public Vector3 endSize = new Vector3(2, 2, 1);
+    
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
@@ -19,10 +24,24 @@ public class CharacterControl : MonoBehaviour
         Instance.currentSelection.text = villager.ToString();
     }
 
+    public void CharacterSizeChange(Single value)
+    {
+        interpolation = value;
+    }
+
+    private void Update()
+    {
+        if (SelectedVillager != null)
+        {
+            SelectedVillager.transform.localScale = Vector3.Lerp(startSize, endSize, (interpolation / 2));
+        }
+    }
+
     private void Start()
     {
         Instance = this;
     }
+
 
     /*private void Update()
     {
